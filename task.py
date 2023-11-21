1e585ab8dadacdd19046e2afb80a6a3ad890fecf
dini <daganidinesh@gmail.com>
2023-11-21 21:10:45 +0530

    add

---
+++ task.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 20:57:35 2023
+
+@author: hp
+"""
+
+#SONGS
+print("nannaku prematho")
+print("samajavaragamana")
+print("maghuva maghuva")
+print("mallela masama")
+print("manasa manasa")
+#MOVIVE
+print("khaidi")
+print("rebal")
+print("racha")
+print("pushpa")
+print("chatrapathi")
